6a6c88f47e64999d5f782ce59aa501d64a915522
AlexKing90210 <Alex.king90@yandex.ru>
2022-08-16 15:05:03 +0400

    Create FileExistException.cs

---
+++ Reader1/FileExistException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IReader
+{
+    public class FileExistException : Exception
+    {
+        public FileExistException(string filePath) : base($"file with path: {filePath} already exist")
+        { }
+    }
+}
